234815440679719d9b37380e176838af58b06f5b
yoonniverse <vnfmadl97@gmail.com>
2023-05-21 03:52:53 +0000

    add debug mode

--- slack/app.py
+++ slack/app.py
@@ -1,32 +1,33 @@
+import os
+import random
+import string
+import datetime
+import time
+import re
+import json
+import subprocess
+from dotenv import load_dotenv
+
 from fastapi import FastAPI, HTTPException, Request, BackgroundTasks
 from fastapi.responses import JSONResponse
 from slack_sdk import WebClient
 from slack_sdk.signature import SignatureVerifier
 from slack_sdk.http_retry.builtin_handlers import RateLimitErrorRetryHandler
-import os
-import random
-import string
-import datetime
-from dotenv import load_dotenv
-import time
-import re
-import json
-import subprocess
+
 load_dotenv('../.env')
 
-# This handler does retries when HTTP status 429 is returned
+app = FastAPI()
+
 rate_limit_handler = RateLimitErrorRetryHandler(max_retry_count=1)
 signature_verifier = SignatureVerifier(
     signing_secret=os.environ["SLACK_SIGNING_SECRET"]
 )
 
-app = FastAPI()
 client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
-# Enable rate limited error retries as well
 client.retry_handlers.append(rate_limit_handler)
 
-def prepare(content):
-    content = re.sub(r'<@U[A-Z0-9]+>', '', content)
+def prepare(user_message):
+    user_message = re.sub(r'<@U[A-Z0-9]+>', '', user_message).strip()
     now = datetime.datetime.now()
     date_str = now.strftime("%Y-%m-%d_%H-%M-%S")
     length = 5
@@ -34,33 +35,43 @@ def prepare(content):
     folder_name = date_str + "_" + random_str
     folder = os.path.join(os.getcwd(), 'auto_gpt_workspace', folder_name)
     os.makedirs(folder, exist_ok=True)
-    ai_settings = f"""ai_name: AutoAskup
-ai_role: an AI that achieves below goals.
+    ai_settings = f"""ai_name: AutoAskUp
+ai_role: an AI that achieves below GOALS.
 ai_goals:
-- {content}
+- {user_message}
 - Terminate if above goal is achieved.
 api_budget: 3"""
     with open(os.path.join(folder, "ai_settings.yaml"), "w") as f:
         f.write(ai_settings)
     return folder
 
-
-def format_output(bytes):
+def format_output(bytes, debug=False):
     text = bytes.decode()
     ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
     text = ansi_escape.sub('', text)
     text = text.strip()
+    if not debug:
+        if text.startswith('SPEAK'):
+            return text[6:].strip()
+        else:
+            return
     index = text.find('THOUGHTS')
     if index != -1:
         return text[index:]
     if "Thinking..." in text:
         return
-    if text.startswith(('REASONING', 'PLAN', '- ', 'CRITICISM', 'SPEAK', 'NEXT ACTION', 'SYSTEM')):
+    prefixes = ('REASONING', 'PLAN', '- ', 'CRITICISM', 'SPEAK', 'NEXT ACTION', 'SYSTEM', '$ SPENT')
+    if text.startswith(prefixes):
         return text
 
 def run_autogpt_slack(data):
     main_dir = os.path.dirname(os.getcwd())
-    folder = prepare(data['event']['text'])
+    user_message = data['event']['text']
+    debug=False
+    if user_message.startswith('!'):
+        debug = True
+        user_message = user_message.replace('!', '').strip()
+    folder = prepare(user_message)
     process = subprocess.Popen(
         ["python", os.path.join(main_dir, 'slack', 'api.py'), os.path.join(main_dir, folder)],
         cwd=main_dir,
@@ -75,16 +86,23 @@ def run_autogpt_slack(data):
         if (not output) and process.poll() is not None:
             break
         if output:
-            print(output.decode().strip())
-            output = format_output(output)
+            output = format_output(output, debug)
             if output is None:
+                continue
+            print(output)
+            if not debug:
+                client.chat_postMessage(
+                    channel=data['event']['channel'],
+                    text=output,
+                    thread_ts=data['event']['ts']
+                )
                 continue
             if output.startswith('THOUGHTS'):
                 started_loop = True
             if not started_loop:
                 continue
             messages.append(output)
-            if started_loop and output.startswith(('NEXT ACTION', 'SYSTEM')):
+            if started_loop and output.startswith(('$ SPENT')):
                 client.chat_postMessage(
                     channel=data['event']['channel'],
                     text="\n".join(messages),
@@ -92,6 +110,15 @@ def run_autogpt_slack(data):
                 )
                 messages = []
         rc = process.poll()
+    if len(messages) > 0:
+        # flush remaining messages
+        client.chat_postMessage(
+            channel=data['event']['channel'],
+            text="\n".join(messages),
+            thread_ts=data['event']['ts']
+        )
+        messages = []
+
     for line in process.stderr:
         print(line.decode().strip())
 
