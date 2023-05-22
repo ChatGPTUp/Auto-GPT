70101ecff2dd2715cfda4f2f364a1adc09e50fc6
yoonniverse <vnfmadl97@gmail.com>
2023-05-22 02:58:02 +0000

    clean code, debug/gpt4mode, print $ spent at last

--- slack/app.py
+++ slack/app.py
@@ -26,35 +26,29 @@ signature_verifier = SignatureVerifier(
 client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
 client.retry_handlers.append(rate_limit_handler)
 
-def prepare(user_message):
-    user_message = re.sub(r'<@U[A-Z0-9]+>', '', user_message).strip()
+def prepare_workspace(user_message):
     now = datetime.datetime.now()
     date_str = now.strftime("%Y-%m-%d_%H-%M-%S")
     length = 5
     random_str = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
-    folder_name = date_str + "_" + random_str
-    folder = os.path.join(os.getcwd(), 'auto_gpt_workspace', folder_name)
-    os.makedirs(folder, exist_ok=True)
+    workspace_name = date_str + "_" + random_str
+    workspace = os.path.join(os.getcwd(), 'auto_gpt_workspace', workspace_name)
+    os.makedirs(workspace, exist_ok=True)
     ai_settings = f"""ai_name: AutoAskUp
 ai_role: an AI that achieves below GOALS.
 ai_goals:
 - {user_message}
 - Terminate if above goal is achieved.
 api_budget: 1"""
-    with open(os.path.join(folder, "ai_settings.yaml"), "w") as f:
+    with open(os.path.join(workspace, "ai_settings.yaml"), "w") as f:
         f.write(ai_settings)
-    return folder
+    return workspace
 
-def format_output(bytes, debug=False):
-    text = bytes.decode()
+def format_stdout(stdout):
+    text = stdout.decode()
     ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
     text = ansi_escape.sub('', text)
     text = text.strip()
-    if not debug:
-        if text.startswith('SPEAK'):
-            return text[6:].strip()
-        else:
-            return
     index = text.find('THOUGHTS')
     if index != -1:
         return text[index:]
@@ -64,73 +58,104 @@ def format_output(bytes, debug=False):
     if text.startswith(prefixes):
         return text
 
-def run_autogpt_slack(data):
-    main_dir = os.path.dirname(os.getcwd())
-    user_message = data['event']['text']
-    debug=False
+def process_user_message(user_message):
+    # Remove @AutoAskUp from message
+    user_message = user_message.replace('<@U058EM1SCEQ>', '').strip()
+    # Extract options from message
+    options = {
+        'debug': False,
+        'gpt3_only': True
+    }
+    if user_message.startswith('?'):
+        options['debug'] = True
+        user_message = user_message.replace('?', '').strip()
     if user_message.startswith('!'):
-        debug = True
+        options['gpt3_only'] = False
         user_message = user_message.replace('!', '').strip()
-    folder = prepare(user_message)
+    return user_message, options
+
+def run_autogpt_slack(user_message, options, channel, thread_ts):
+    
+    # Make workspace folder and write ai_settings.yaml in it
+    workspace = prepare_workspace(user_message)
+    
+    # Run autogpt
+    main_dir = os.path.dirname(os.getcwd())
     process = subprocess.Popen(
-        ["python", os.path.join(main_dir, 'slack', 'api.py'), os.path.join(main_dir, folder)],
+        ["python", os.path.join(main_dir, 'slack', 'api.py'), os.path.join(main_dir, workspace), str(options['gpt3_only'])],
         cwd=main_dir,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
 
+    # Read stdout and send messages to slack
     started_loop = False
     messages = []
+    dollars_spent = 0
     while True:
-        output = process.stdout.readline()
-        if (not output) and process.poll() is not None:
+        stdout = process.stdout.readline()
+        if (not stdout) and process.poll() is not None:
             break
-        if output:
-            output = format_output(output, debug)
-            if output is None:
-                continue
-            print(output)
-            if not debug:
+        if not stdout:
+            continue
+        output = format_stdout(stdout)
+        if output is None:
+            continue
+        print(output)
+        if output.startswith('$ SPENT'):
+            dollars_spent = output.split('$ SPENT:')[1].strip()
+        if not options['debug']:
+            if output.startswith('SPEAK'):
+                output = output[6:].strip()
                 client.chat_postMessage(
-                    channel=data['event']['channel'],
+                    channel=channel,
                     text=output,
-                    thread_ts=data['event']['ts']
+                    thread_ts=thread_ts
                 )
-                continue
-            if output.startswith('THOUGHTS'):
-                started_loop = True
-            if not started_loop:
-                continue
-            messages.append(output)
-            if started_loop and output.startswith(('$ SPENT')):
-                client.chat_postMessage(
-                    channel=data['event']['channel'],
-                    text="\n".join(messages),
-                    thread_ts=data['event']['ts']
-                )
-                messages = []
+            continue
+        if output.startswith('THOUGHTS'):
+            started_loop = True
+        if not started_loop:
+            continue
+        messages.append(output)
+        if started_loop and output.startswith(('$ SPENT')):
+            client.chat_postMessage(
+                channel=channel,
+                text="\n".join(messages),
+                thread_ts=thread_ts
+            )
+            messages = []
         rc = process.poll()
     if len(messages) > 0:
-        # flush remaining messages
+        # Send remaining messages to slack
         client.chat_postMessage(
-            channel=data['event']['channel'],
+            channel=channel,
             text="\n".join(messages),
-            thread_ts=data['event']['ts']
+            thread_ts=thread_ts
         )
         messages = []
 
+    # Print stderr
     for line in process.stderr:
         print(line.decode().strip())
 
-    for fname in os.listdir(folder):
+    # Upload files to slack
+    for fname in os.listdir(workspace):
         if fname not in ['ai_settings.yaml', 'auto-gpt.json', 'file_logger.txt']:
-            file = os.path.join(folder, fname)
+            file = os.path.join(workspace, fname)
             upload_text_file = client.files_upload(
-                channels=data['event']['channel'],
-                thread_ts=data['event']['ts'],
+                channels=channel,
+                thread_ts=thread_ts,
                 title=fname,
                 file=file,
             )
+
+    # Send $ spent message to slack
+    client.chat_postMessage(
+        channel=channel,
+        text=f"Total $ spent: {dollars_spent}",
+        thread_ts=thread_ts
+    )
 
 @app.post("/")
 async def slack_events(request: Request, background_tasks: BackgroundTasks):
@@ -153,10 +178,16 @@ async def slack_events(request: Request, background_tasks: BackgroundTasks):
         raise HTTPException(status_code=401, detail="Invalid signature")
 
     data = json.loads(body)
-    background_tasks.add_task(run_autogpt_slack, data)
+    user_message, options = process_user_message(data['event']['text'])
+    background_tasks.add_task(run_autogpt_slack, user_message, options, data['event']['channel'], data['event']['ts'])
+    start_message = "AutoGPT가 실행됩니다."
+    if options['debug']:
+        start_message += " (DEBUG MODE)"
+    if not options['gpt3_only']:
+        start_message += " (GPT-4)"
     client.chat_postMessage(
         channel=data['event']['channel'],
-        text="AutoGPT가 실행됩니다.",
+        text=start_message,
         thread_ts=data['event']['ts']
     )
     return JSONResponse(content="Launched AutoGPT.")
